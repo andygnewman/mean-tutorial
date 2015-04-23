db379427e61bf6a35d0352ecee4cb72f9e7082bf
Andy <andygnewman@gmail.com>
2015-04-23 17:39:47 +0100

    tidy up environments

--- test/e2e/homeSpec.js
+++ test/e2e/homeSpec.js
@@ -1,6 +1,23 @@
 describe("Andy's first angular app", function() {
-  it('should have "Hello Andy!" on the page', function() {
+
+  beforeEach(function() {
     browser.get('app/index.html');
+  });
+
+  it('should have "Hello Andy!" on the page', function() {
     expect(element(by.binding('test')).getText()).toBe('Hello Andy!');
   });
+
+  it('should have posts 1 to 5 on the page', function() {
+    var posts = element.all(by.repeater('post in posts'));
+
+    function getPostsText() {
+      return posts.map(function(elm) {
+        return elm.getText();
+      });
+    }
+
+    expect(getPostsText()).toEqual(['post1', 'post2', 'post3', 'post4', 'post5']);
+  });
+
 });
