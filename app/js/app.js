44473d077cfef22f305413b303affc1ad48e4704
Andy <andygnewman@gmail.com>
2015-04-27 14:02:58 +0100

    create factory for posts and include ui router

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,21 @@
-var app = angular.module('andyNews', []);
+var app = angular.module('andyNews', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', function($scope) {
+app.factory('posts', [function() {
+  var o = {
+    posts: [
+            {title: 'post1', upvotes: 5},
+            {title: 'post2', upvotes: 2},
+            {title: 'post3', upvotes: 15},
+            {title: 'post4', upvotes: 9},
+            {title: 'post5', upvotes: 4}
+    ]
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
     $scope.test = "Hello Andy!";
-    $scope.posts = [
-      {title: 'post1', upvotes: 5},
-      {title: 'post2', upvotes: 2},
-      {title: 'post3', upvotes: 15},
-      {title: 'post4', upvotes: 9},
-      {title: 'post5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
     $scope.addPost = function() {
       if(!$scope.title || $scope.title === '') {return;}
       $scope.posts.push({
